98664712d3151f8015a911fe867a69c6def1c120
Optick002 <pauliussuku@gmail.com>
2026-06-16 23:24:00 +0300

    wip

--- modules/scala-native-gdextension/src/main/scala/io/github/optical002/godot/GodotEntry.scala
+++ modules/scala-native-gdextension/src/main/scala/io/github/optical002/godot/GodotEntry.scala
@@ -2,17 +2,24 @@ package io.github.optical002.godot
 
 import scala.scalanative.unsafe.*
 import scala.scalanative.unsigned.*
+import io.github.optical002.godot.codegen.gdextensioninterface.codegen.types.Interface
+import io.github.optical002.godot.codegen.gdextensioninterface.types.*
 
 object GodotEntry {
   @exported("godot_scala_init")
   def godotScalaInit(
-    getProcAddressRaw: Ptr[Byte],
-    libraryRaw: Ptr[Byte],
-    initPtr: Ptr[Byte]
+    getProcAddress: GDExtensionInterfaceGetProcAddress,
+    library: GDExtensionClassLibraryPtr,
+    r_initialization: Ptr[GDExtensionInitialization]
   ): CUnsignedChar = FileLogger.use("godot-init") { logger =>
     logger.log(
       "Starting Scala-Native language binding."
     )
+    try {
+      val interface = Interface.load(getProcAddress)
+    } catch {
+      case e: Exception => logger.log(e.toString)
+    }
 
     1.toUByte // Success
   }
